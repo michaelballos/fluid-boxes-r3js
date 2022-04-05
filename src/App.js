baaf62d0ce39fa15bd41f51fd3b59981a55c3464
michaelballos <mdb3091207@gmail.com>
2022-04-05 12:42:04 -0500

    Make
    3 boxes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import { Canvas } from '@react-three/fiber';
 import BoxOne from './components/BoxOne.tsx';
 
 const App = () => {
+  
   return (
-      <Canvas>
-        <BoxOne />
+      <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}> 
+        <ambientLight intensity={0.3} />
+        <BoxOne position={[0, 1, 0]} args={[3, 2, 1]} />
+        <BoxOne position={[-2, 1, -5]}/>
+        <BoxOne position={[5, 1, -2]}/>
       </Canvas>
+
   );
 }
 
--- src/components/BoxOne.tsx
+++ src/components/BoxOne.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import { useFrame } from '@react-three/fiber'
+import React, { useRef } from 'react'
 
-const BoxOne: React.FC = () => {
+const BoxOne: React.FC = ({ position }) => {
+  const Mesh = useRef(null);
+  useFrame(() => { Mesh.current.rotation.x = Mesh.current.rotation.y += 0.01 });
   return (
-    <mesh>
+    <mesh position={position} ref={Mesh}>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshLambertMaterial attach="material" color="red" />
+      <meshLambertMaterial attach="material" color="lightBlue" />
     </mesh>
   )
 }
