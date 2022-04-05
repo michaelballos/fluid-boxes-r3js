a277fa54a8a069178b0534ae6235b06dd578fe3e
michaelballos <mdb3091207@gmail.com>
2022-04-05 12:49:52 -0500

    Add
    Args type

--- src/components/BoxOne.tsx
+++ src/components/BoxOne.tsx
@@ -1,12 +1,13 @@
 import { useFrame } from '@react-three/fiber'
 import React, { useRef } from 'react'
+import { IPosition } from '../../types'
 
-const BoxOne: React.FC = ({ position }) => {
+const BoxOne: React.FC<{ position: IPosition, arge: IArgs }> = ({ position, args }) => {
   const Mesh = useRef(null);
   useFrame(() => { Mesh.current.rotation.x = Mesh.current.rotation.y += 0.01 });
   return (
     <mesh position={position} ref={Mesh}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <boxBufferGeometry attach="geometry" args={args} />
       <meshLambertMaterial attach="material" color="lightBlue" />
     </mesh>
   )
---
+++ types.tsx
@@ -0,0 +1,11 @@
+export interface IPosition {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface IArgs {
+  width: number;
+  height: number;
+  depth: number;
+}
