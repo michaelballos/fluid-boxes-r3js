cd8b3d8c7915801544b2affb207161a2ebabeb52
michaelballos <mdb3091207@gmail.com>
2022-04-05 12:56:28 -0500

    Change
    Color of back two boxes to pink

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ const App = () => {
   return (
       <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}> 
         <ambientLight intensity={0.3} />
+        <pointLight position={[-10, 0, -20]} intensity={.5} />
+        <pointLight position={[0, 1, 10]} intensity={1.5} />
         <BoxOne position={[0, 1, 0]} args={[3, 2, 1]} />
-        <BoxOne position={[-2, 1, -5]}/>
-        <BoxOne position={[5, 1, -2]}/>
+        <BoxOne position={[-2, 1, -5]} color='pink' />
+        <BoxOne position={[5, 1, -2]} color='pink' />
       </Canvas>
 
   );
--- src/components/BoxOne.tsx
+++ src/components/BoxOne.tsx
@@ -1,14 +1,14 @@
 import { useFrame } from '@react-three/fiber'
 import React, { useRef } from 'react'
-import { IPosition } from '../../types'
+import { IPosition, IArgs } from '../../types'
 
-const BoxOne: React.FC<{ position: IPosition, arge: IArgs }> = ({ position, args }) => {
+const BoxOne: React.FC<{ position: IPosition, args: IArgs, color: string }> = ({ position, args, color }) => {
   const Mesh = useRef(null);
   useFrame(() => { Mesh.current.rotation.x = Mesh.current.rotation.y += 0.01 });
   return (
     <mesh position={position} ref={Mesh}>
       <boxBufferGeometry attach="geometry" args={args} />
-      <meshLambertMaterial attach="material" color="lightBlue" />
+      <meshLambertMaterial attach="material" color={color} />
     </mesh>
   )
 }
